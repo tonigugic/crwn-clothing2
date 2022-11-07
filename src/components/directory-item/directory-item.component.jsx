b7ac832118d70c667bedbd35f328918327fbf1db
tonigugic <toni.gugic@gmail.com>
2022-11-07 15:35:04 +0100

    testing staging branch

--- src/components/directory-item/directory-item.component.jsx
+++ src/components/directory-item/directory-item.component.jsx
@@ -11,7 +11,7 @@ const DirectoryItem = ({ category }) => {
 			<BackgroundImage imageUrl={imageUrl} />
 			<Body>
 				<h2>{title}</h2>
-				<p>Shop Now</p>
+				<p>Shop Now Baby</p>
 			</Body>
 		</DirectoryItemContainer>
 	);
